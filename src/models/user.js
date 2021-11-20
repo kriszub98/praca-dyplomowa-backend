fd7d20fb4d9989ce8d117be8e8b81469518468e0
kzubrzycki <kriszub98@gmail.com>
2021-11-20 19:18:46 +0100

    Setting up connection and user model

---
+++ src/models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+	login: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+		minLength: 8
+	},
+	tokens: [
+		{
+			token: {
+				type: String,
+				required: true
+			}
+		}
+	]
+});
+
+userSchema.methods.generateAuthToken = async function() {
+	const user = this;
+	const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+
+	user.tokens = user.tokens.concat({ token });
+	await user.save();
+
+	return token;
+};
+
+// Hashing Plane Password
+userSchema.pre('save', async function(next) {
+	const user = this;
+
+	if (user.isModified('password')) {
+		user.password = await bcrypt.hash(user.password, 8);
+	}
+
+	next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+
+// TODO: VALIDATE PASS
+// TODO: BLOCKED & FAVS DISHES
